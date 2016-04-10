475832087c1fd240fb489027b9c6a5779d813014
Oscar Nilsson <oscar@oncolour.se>
2016-04-10 16:57:34 -0600

    fixing local/prod resources and adding .htaccess file

--- includes/_page_footer.php
+++ includes/_page_footer.php
@@ -1,16 +1,17 @@
 
 <!-- Scripts -->
-
+<?php if($local = $_SERVER['REMOTE_ADDR']=='::1' ? 1 : 0) { ?>
 <!-- Development -->
 <script type="text/javascript" src="js/vendor/foundation.min.js"></script>
 <script type="text/javascript" src="js/vendor/motion-ui.min.js"></script>
 <script type="text/javascript" src="src/javascripts/gdservice.js"></script>
-
-<!-- Production 
+  <?php } else {?>
+<!-- Production -->
 <script type="text/javascript" src="js/vendor/foundation.min.js"></script>
 <script type="text/javascript" src="js/vendor/motion-ui.min.js"></script>
 <script type="text/javascript" src="js/gdservice.min.js"></script>
 <!-- End Production imports -->
+<?php } ?>
 </body>
 
 </html>
