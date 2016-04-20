ced0009defbfdd024ab2ee088544b3d971c3ce13
Oscar Nilsson <oscar@intoria.com>
2016-04-20 09:07:36 -0600

    Recompile and adding lots of meta tags

--- includes/_page_header.php
+++ includes/_page_header.php
@@ -1,15 +1,21 @@
-<!doctype html>
-<html class="no-js" lang="en">
-
+<!DOCTYPE html>
+<html class="no-js" lang="sv_SE" prefix="og: http://ogp.me/ns#">
 <head>
+    <title>Busstvätt och Chaufförsbemanning - GDService</title>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Busstvätt och Chaufförsbemanning - GD Service</title>
-    <meta name="description" content="GD Service erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm.">
+    <meta name="description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm."/>    
+    <meta property="og:locale" content="sv_SE" />
+	<meta property="og:type" content="website" />
+    <meta property="og:title" content="Busstvätt och Chaufförsbemanning - GDService" />
+	<meta property="og:description" content="description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm." />
+	<meta property="og:url" content="http://www.gdservice.se" />
+	<meta property="og:site_name" content="GDservice" />
+ 	<meta property="og:image" content="http://www.gdservice.se/src/images/gdservice-logo.png" />
+    <meta name="application-name" content="GDservice" />
     
-    <?php // Check REMOTE_ADDR  phpinfo(); ?>
-    
+    <?php // Check REMOTE_ADDR  phpinfo(); ?>  
     <?php if($local = $_SERVER['REMOTE_ADDR']=='::1' ? 1 : 0) { ?>
     <!-- Development -->
     <link rel="stylesheet" href="src/_build/f6.css">
@@ -23,7 +29,6 @@
     <link rel="stylesheet" href="css/gdservice.min.css">
     <!-- End Production imports -->
     <?php } ?>
-
 </head>
 
 <body>
