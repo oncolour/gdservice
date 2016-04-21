f1e9daf36c9ccf641b7d6c8091ee47655db202ee
Oscar Nilsson <oscar@oncolour.se>
2016-04-20 21:02:51 -0600

    adding micro data

--- includes/_page_header.php
+++ includes/_page_header.php
@@ -1,19 +1,23 @@
 <!DOCTYPE html>
-<html class="no-js" lang="sv" prefix="og: http://ogp.me/ns#">
+<html class="no-js" lang="sv" itemscope itemtype="http://schema.org/LocalBusiness" prefix="og: http://ogp.me/ns#">
+
 <head>
     <title>Busstvätt och Chaufförsbemanning - GDService</title>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm."/>    
+    <meta name="description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm."/> 
+    <meta itemprop="name" content="GDService">
+    <meta itemprop="description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm.">       
     <meta property="og:locale" content="sv_SE" />
 	<meta property="og:type" content="website" />
-    <meta property="og:title" content="Busstvätt och Chaufförsbemanning - GDService" />
+    <meta property="og:title" content="GDService" />
 	<meta property="og:description" content="GDService erbjuder högkvalitativ busstvätt, service och underhåll av turistbussar samt chaufförsbemanning inom Sverige med utgångspunkt i Stockholm." />
 	<meta property="og:url" content="http://www.gdservice.se" />
 	<meta property="og:site_name" content="GDservice" />
  	<meta property="og:image" content="http://www.gdservice.se/src/images/gdservice-logo.png" />
     <meta name="application-name" content="GDservice" />
+    
     
     <?php // Check REMOTE_ADDR  phpinfo(); ?>  
     <?php if($local = $_SERVER['REMOTE_ADDR']=='::1' ? 1 : 0) { ?>
