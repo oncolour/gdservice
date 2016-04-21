958af0616a3ee096a0e0024af545cf8d18c08e76
Oscar Nilsson <oscar@oncolour.se>
2016-04-20 22:52:50 -0600

    centering hero content section and increasing spacing between CTA links

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
                 <h1>Busstvätt &amp; Chaufförsbemanning i Stockholm.</h1>
                 <h2>Håll dina bussar tvättade.</h2>
                 <p>Vi erbjuder hög kvalitativ busstvätt och städservice för turistbussar i Sverige, med utgångspunkt Stockholm.</p>
-                <span itemscope itemtype="http://schema.org/Offer"><a itemprop="url" class="button" href="mailto:daniel@gdservice.se?subject=Kontakt%20från%20GDservice.se"><span itemprop="description">Få din buss tvättad idag</span></a></span>
+                <div><span itemscope itemtype="http://schema.org/Offer"><a itemprop="url" class="button" href="mailto:daniel@gdservice.se?subject=Kontakt%20från%20GDservice.se"><span itemprop="description">Få din buss tvättad idag</span></a></span>
                 <a class="text-center" href="#services">Vår service</a>
+                </div>
             </section>
         </div>
     </div>
